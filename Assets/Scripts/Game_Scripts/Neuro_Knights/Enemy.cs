8f7fc22feb1788f86e47cf4719af9e0757985229
cetinye <cetinye@hotmail.com>
2024-07-03 15:47:15 +0300

    visual bug fix

--- Assets/Scripts/Game_Scripts/Neuro_Knights/Enemy.cs
+++ Assets/Scripts/Game_Scripts/Neuro_Knights/Enemy.cs
@@ -44,7 +44,8 @@ namespace Neuro_Knights
 
 		private void FollowPlayer()
 		{
-			transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+			// transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, -0.004f), speed * Time.deltaTime);
 			LookAtPlayer();
 		}
 
