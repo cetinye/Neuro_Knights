d1a72a78c767fbd67b7186e368dcd6687bad23a8
cetinye <cetinye@hotmail.com>
2024-07-03 12:02:17 +0300

    Joystick and player movement added

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody2D rb;
+
+    public void Update()
+    {
+        Vector3 direction = Vector3.up * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        transform.position += speed * Time.deltaTime * direction.normalized;
+    }
+}
