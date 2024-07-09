77a5e18ab2c64d3647c8c799ad9fbc54e5bc054b
cetinye <cetinye@hotmail.com>
2024-07-09 11:48:33 +0300

    camera clamp

--- Assets/Scripts/Game_Scripts/Neuro_Knights/FollowPlayer.cs
+++ Assets/Scripts/Game_Scripts/Neuro_Knights/FollowPlayer.cs
@@ -7,10 +7,12 @@ namespace Neuro_Knights
 	public class FollowPlayer : MonoBehaviour
 	{
 		[SerializeField] private Player player;
+		[SerializeField] private float xBound;
+		[SerializeField] private float yBound;
 
 		void LateUpdate()
 		{
-			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+			transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, -xBound, xBound), Mathf.Clamp(player.transform.position.y, -yBound, yBound), -10f);
 		}
 	}
 }
